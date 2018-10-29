c8c44fd4adf8c1babebdee9783b19cd8660d4f23
Sharmina Akhtar <sakht318@gmail.com>
2018-10-29 16:25:11 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={store}>
   <App />
+  </Provider>,
   // add imports and code
   ,
   document.getElementById('root')
